de577d1fb570e11bc89b808a0df1bc687da60feb
Victor Mican <48836289+victormican@users.noreply.github.com>
2025-12-18 17:12:20 -0500

    Update test.js

--- k6/test.js
+++ k6/test.js
@@ -3,13 +3,10 @@ import { sleep } from 'k6';
 
 export const options = {
   vus: 5,
-  duration: '15s',
+  duration: '20s',
 };
 
 export default function () {
   const res = http.get('https://httpbin.org/get');
-  if (res.status !== 200) {
-    throw new Error('Request failed');
-  }
   sleep(1);
 }
