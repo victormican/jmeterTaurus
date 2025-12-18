70518d3b4a06e8b0947eec0978ad06a5171af3b0
Victor Mican <48836289+victormican@users.noreply.github.com>
2025-12-18 17:06:26 -0500

    Update test.js

--- k6/test.js
+++ k6/test.js
@@ -1,21 +1,12 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { sleep } from 'k6';
 
 export const options = {
-  vus: __ENV.USUARIOS ? parseInt(__ENV.USUARIOS) : 10,
-  duration: __ENV.DURACION ? `${__ENV.DURACION}s` : '60s',
-  thresholds: {
-    http_req_failed: ['rate<0.01'],
-    http_req_duration: ['p(95)<2000'],
-  },
+  vus: 5,
+  duration: '10s',
 };
 
 export default function () {
-  const res = http.get('https://example.com');
-
-  check(res, {
-    'status is 200': (r) => r.status === 200,
-  });
-
+  http.get('https://httpbin.org/get');
   sleep(1);
 }
