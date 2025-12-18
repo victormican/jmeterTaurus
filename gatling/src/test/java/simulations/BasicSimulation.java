058b718d9d07bf279825802c3644d5dd9aa7764f
Victor Mican <48836289+victormican@users.noreply.github.com>
2025-12-18 16:11:01 -0500

    Create BasicSimulation.java

---
+++ gatling/src/test/java/simulations/BasicSimulation.java
@@ -0,0 +1,34 @@
+package simulations;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class BasicSimulation extends Simulation {
+
+    HttpProtocolBuilder httpProtocol =
+        http.baseUrl("https://example.com")
+            .acceptHeader("application/json");
+
+    ScenarioBuilder scn =
+        scenario("Basic Load Test")
+            .exec(
+                http("GET Home")
+                    .get("/")
+                    .check(status().is(200))
+            );
+
+    {
+        setUp(
+            scn.injectOpen(
+                rampUsers(
+                    Integer.getInteger("USUARIOS", 10)
+                ).during(
+                    Integer.getInteger("RAMP_UP", 30)
+                )
+            )
+        ).protocols(httpProtocol);
+    }
+}
